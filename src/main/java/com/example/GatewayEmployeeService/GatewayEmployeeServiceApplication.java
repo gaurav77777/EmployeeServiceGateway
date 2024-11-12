3ee65cddbaf086b587ef26878ce9299652403ba8
gaurav77777 <lallgaurav538@gmail.com>
2024-11-12 17:18:57 +0530

    Initial commit

---
+++ src/main/java/com/example/GatewayEmployeeService/GatewayEmployeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.GatewayEmployeeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayEmployeeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GatewayEmployeeService/GatewayEmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GatewayEmployeeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayEmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
